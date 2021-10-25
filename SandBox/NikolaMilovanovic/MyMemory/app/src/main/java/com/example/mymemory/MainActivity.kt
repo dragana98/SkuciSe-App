013264083792dc62f3ac65ff3ca0034666623e61
hsongcloud <haichen.s@cloudengage.com>
2021-10-25 21:41:20 +0200

    MyMemory app za vezbanje Kotlina

---
+++ SandBox/NikolaMilovanovic/MyMemory/app/src/main/java/com/example/mymemory/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.mymemory
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemory.models.BoardSize
+import com.example.mymemory.models.MemoryGame
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+    private lateinit var clRoot: ConstraintLayout
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
+    private var boardSize: BoardSize = BoardSize.EASY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clRoot = findViewById(R.id.clRoot)
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+        setUpBoard()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh -> {
+                if(memoryGame.getNumMoves() > 0 && !memoryGame.haveWon()){
+                    showAlertDialog("Quit your current game?", null, View.OnClickListener {
+                        setUpBoard()
+                    })
+                } else {
+                    setUpBoard()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK"){ _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
+    private fun setUpBoard(){
+        when(boardSize){
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "Medium: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 8"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Medium: 6 x 4"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        if(memoryGame.haveWon()){
+            Snackbar.make(clRoot, "You already have won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if(memoryGame.isCardFaceUp(position)){
+            Snackbar.make(clRoot, "Invalid Move!", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        if(memoryGame.flipCard(position)){
+            Log.i(TAG, "Found a match! Num pairs found: ${memoryGame.numPairsFound}")
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() /boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+            ) as Int
+            tvNumPairs.setTextColor(color)
+            tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()}"
+            if(memoryGame.haveWon()){
+                Snackbar.make(clRoot, "You won! Congratulations.", Snackbar.LENGTH_LONG).show()
+            }
+        }
+        tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
+        adapter.notifyDataSetChanged()
+    }
+}
