405ffce624d6a059e9727ffaa9a48cf320fc0fc3
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2021-10-26 12:39:30 +0200

    Add projects

---
+++ SandBox/DraganaAndjelkovic/FirstApplication/Aplikacija1/app/src/main/java/com/example/aplikacija1/SecondCrveni.kt
@@ -0,0 +1,41 @@
+package com.example.aplikacija1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.aplikacija1.databinding.FragmentFirstBinding
+
+class SecondCrveni : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
