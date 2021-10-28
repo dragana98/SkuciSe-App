63818d05e73f3085c28b77c58d9b9c385cbba8e1
Krunoslav Velfel <kvelfel@gmail.com>
2021-10-28 05:15:39 +0200

    #7 Dodat top bar

--- src/SkuciSe/app/src/main/java/com/blackbyte/skucise/screens/HomeScreen.kt
+++ src/SkuciSe/app/src/main/java/com/blackbyte/skucise/screens/HomeScreen.kt
@@ -2,11 +2,16 @@ package com.blackbyte.skucise.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -17,15 +22,27 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.blackbyte.skucise.R
+import androidx.compose.material.TextField
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.input.VisualTransformation
+import com.blackbyte.skucise.components.OutlinedInputField
 import com.blackbyte.skucise.ui.theme.SkuciSeTheme
+import kotlinx.coroutines.launch
 
 @Preview
 @Composable
 fun HomeScreen() {
     val gradient = Brush.linearGradient(0f to Color.Magenta, 1000f to Color.Yellow)
+    val state = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    var query by remember { mutableStateOf("") }
+
 
     SkuciSeTheme {
         Scaffold(
+            scaffoldState = state,
             drawerContent = {
                 Text("Drawer title", modifier = Modifier.padding(16.dp))
                 Divider()
@@ -53,6 +70,53 @@ fun HomeScreen() {
                             Text("Skući Se", fontWeight = FontWeight.Bold)
                         }
 
+                        Spacer(modifier = Modifier.size(20.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+                                contentPadding = PaddingValues(all = 8.dp),
+                                modifier = Modifier
+                                    .size(48.dp),
+                                onClick = {
+                                    scope.launch { if (state.drawerState.isClosed) state.drawerState.open() else state.drawerState.close() }
+                                }) {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "menu icon",
+                                    tint = Color.Black,
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                )
+                            }
+                            Spacer(modifier = Modifier.size(20.dp))
+                            TextField(
+                                colors = TextFieldDefaults.textFieldColors(
+                                    backgroundColor = Color.White,
+                                    cursorColor = Color.Black,
+                                    disabledLabelColor = Color.White,
+                                    focusedIndicatorColor = Color.Transparent,
+                                    unfocusedIndicatorColor = Color.Transparent
+                                ),
+                                singleLine = true,
+                                value = query,
+                                onValueChange = { query = it },
+                                leadingIcon = {Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "menu icon",
+                                    tint = Color.Black
+                                )},
+                                label = {
+                                    Text("Pretraga...")
+                                },
+                                textStyle = LocalTextStyle.current,
+                                shape = RoundedCornerShape(4.dp),
+                                modifier = Modifier
+                                    .height(48.dp)
+                                    .fillMaxWidth()
+                            )
+                        }
                     }
                 }
             },
