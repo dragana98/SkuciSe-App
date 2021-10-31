aef8fd99784acd3bc152c56700a58d1d97104726
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2021-10-31 13:02:36 +0100

    Class added

---
+++ src/SkuciSe/app/src/main/java/com/blackbyte/skucise/components/Ad.kt
@@ -0,0 +1,9 @@
+package com.blackbyte.skucise.components
+
+class Ad(adName: String, price: String, details: String)
+{
+    val adName = adName
+    val price = price
+    val details = details
+
+}
